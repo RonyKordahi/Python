efd65c630b344fb7f18568e1b7be6d9ef6e08f8a
RonyKordahi <kordrony@gmail.com>
2020-07-13 12:15:51 -0400

    Module done

---
+++ Section 10 - Errors and Exceptions Handling/simple1.py
@@ -0,0 +1,16 @@
+'''
+#DOCSTRING
+A very simple script
+'''
+
+def myfunc():
+    """
+    A simple function
+    """
+    first = 1
+    second = 2
+
+    print(first)
+    print(second)
+
+myfunc()
